6d4bf571fed1640c95c6df135af393faab4166c9
Meiman Jaya Gea <meimanjayagea@Pro.local>
2024-09-14 22:10:32 +0700

    update fix

--- src/main/java/com/learn/app/book/BookController.java
+++ src/main/java/com/learn/app/book/BookController.java
@@ -13,9 +13,7 @@ public class BookController {
     private final BookService service;
 
     @PostMapping
-    public ResponseEntity<?> save(
-            @RequestBody BookRequest request
-    ) {
+    public ResponseEntity<?> save(@RequestBody BookRequest request) {
         service.save(request);
         return ResponseEntity.accepted().build();
     }
